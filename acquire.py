016188a48dbfcf66a3d25f80660c32849e946fa6
Matthew-Mays <matthew.c.mays96@gmail.com>
2020-12-10 09:21:53 -0600

    added function to acquire data from playlist

---
+++ acquire.py
@@ -0,0 +1,34 @@
+# Function to acquire playlist tracks and features
+def analyze_playlist(creator, playlist_id):
+    
+    # Create empty dataframe
+    playlist_features_list = ["artist","album","track_name","track_id","danceability","energy","key",
+                              "loudness","mode", "speechiness","instrumentalness","liveness","valence",
+                              "tempo", "duration_ms","time_signature"]
+    
+    playlist_df = pd.DataFrame(columns = playlist_features_list)
+    
+    # Loop through every track in the playlist, extract features and append the features to the playlist df
+    
+    playlist = sp.user_playlist_tracks(creator, playlist_id)["tracks"]['items']
+    for track in playlist:
+        # Create empty dict
+        playlist_features = {}
+        # Get metadata
+        playlist_features["artist"] = track["track"]["album"]["artists"][0]["name"]
+        playlist_features["album"] = track["track"]["album"]["name"]
+        playlist_features["release_date"] = track["track"]["album"]["release_date"]
+        playlist_features["track_name"] = track["track"]["name"]
+        playlist_features["track_id"] = track["track"]["id"]
+        playlist_features["popularity"] = track["track"]["popularity"]
+        
+        # Get audio features
+        audio_features = sp.audio_features(playlist_features["track_id"])[0]
+        for feature in playlist_features_list[4:]:
+            playlist_features[feature] = audio_features[feature]
+        
+        # Concat the dfs
+        track_df = pd.DataFrame(playlist_features, index = [0])
+        playlist_df = pd.concat([playlist_df, track_df], ignore_index = True)
+        
+    return playlist_df
