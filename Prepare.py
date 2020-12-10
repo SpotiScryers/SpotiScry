818a1fb44c0f24d8436b6d2d53cf579f6d72a597
Kwame Taylor <maxkwametaylor@gmail.com>
2020-12-10 10:47:51 -0600

    add handle nulls

--- Prepare.py
+++ Prepare.py
@@ -3,7 +3,15 @@
 
 import pandas as pd
 
-####################################################### Fill Nulls #######################################################
+###################################################### Handle Nulls ######################################################
+
+def handle_nulls(df):
+    '''
+    This function takes in a DataFrame and returns a DataFrame with the nulls handled.
+    Release dates that don't have a month or day are assign a day and month of '01-01'.
+    '''
+    df['release_date'] = np.where(df['release_date'].str.len()==4, df.release_date.astype(str) + '-01-01', df['release_date'])
+    return df
 
 ##################################################### Split the Data #####################################################
 
