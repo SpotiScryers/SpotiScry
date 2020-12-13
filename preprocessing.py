d77989d99a34f571d7c14e5d4f65fde27868dcf7
Kwame Taylor <maxkwametaylor@gmail.com>
2020-12-13 14:33:31 -0600

    added y_train, y_validate, y_test to preprocessing for modeling

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@
 # Functions to reproduce the pre-processing of the data
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 ################################################## Feature Engineering ###################################################
 
@@ -79,6 +80,36 @@ def split_df(df):
             ", test percent: ", test_percent)
 
     return train, validate, test
+
+def spotify_split(df, target):
+    '''
+    This function takes in a dataframe and splits it into
+    test (15%), validate (15%), and train (70%). 
+    It also splits test, validate, and train into X and y dataframes.
+    Returns X_train, y_train, X_validate, y_validate, X_test, y_test, train, validate, test.
+    '''
+    # split df into test (15%) and train_validate (85%)
+    train_validate, test = train_test_split(df, test_size=.15, random_state=666) #stratify=df[target]
+
+    # split train_validate off into train (82.35% of 85% = 70%) and validate (17.65% of 85% = %15)
+    train, validate = train_test_split(train_validate, test_size=.1765, random_state=666)
+        
+    # split train into X & y
+    X_train = train.drop(columns=[target])
+    y_train = train[target]
+    
+    # split validate into X & y
+    X_validate = validate.drop(columns=[target])
+    y_validate = validate[target]
+    
+    # split test into X & y
+    X_test = test.drop(columns=[target])
+    y_test = test[target]
+    
+    print('Shape of train:', X_train.shape, '| Shape of validate:', X_validate.shape, '| Shape of test:', X_test.shape)
+    print('Percent train:', round(((train.shape[0])/df.count()[0]),2) * 100, '| Percent validate:', round(((validate.shape[0])/df.count()[0]),2) * 100, '| Percent test:', round(((test.shape[0])/df.count()[0]),2) * 100)
+
+    return X_train, y_train, X_validate, y_validate, X_test, y_test, train, validate, test
 
 ##################################################### Scale the Data #####################################################
 
