0ee01596f2e75700d978b435f965787c7fd475ec
Kwame Taylor <maxkwametaylor@gmail.com>
2020-12-13 16:07:42 -0600

    adjust function to new index

--- model.py
+++ model.py
@@ -5,5 +5,5 @@ def get_model_features(df):
     '''
     This function takes in a DataFrame and returns a DataFrame with features to use in predictive modeling.
     '''
-    df = df.drop(columns=['artist', 'album', 'release_date', 'track_name', 'track_id'])
+    df = df.drop(columns=['artist', 'album', 'release_date', 'track_name'])
     return df
