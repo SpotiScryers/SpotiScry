c334441c440cb06a68df3676660a27f75c96c7ee
Kwame Taylor <maxkwametaylor@gmail.com>
2020-12-13 15:22:21 -0600

    fix handle nulls function

--- prepare.py
+++ prepare.py
@@ -1,4 +1,4 @@
-# Module: Prepare.py
+# Module: prepare.py
 # Functions to prep data acquired from the Spotify API
 
 import pandas as pd
@@ -11,5 +11,10 @@ def handle_nulls(df):
     This function takes in a DataFrame and returns a DataFrame with the nulls handled.
     Release dates that don't have a month or day are assign a day and month of '01-01'.
     '''
+    # handle nulls in release data
     df['release_date'] = np.where(df['release_date'].str.len()==4, df.release_date.astype(str) + '-01-01', df['release_date'])
+
+    # drop observations that contain nulls
+    df = df.dropna()
+
     return df
